f13b796ec6bb06c838a5097ee20141d9505939f0
loopercamera <fabian.gross@students.fhnw.ch>
2024-05-30 13:55:50 +0200

    Add square to map

--- frontend/src/test_gross.js
+++ frontend/src/test_gross.js
@@ -2,18 +2,24 @@ import React, { useRef, useEffect } from "react";
 import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import OSM from "ol/source/OSM";
-import { fromLonLat, toLonLat } from "ol/proj";
-import { Vector as VectorLayer } from "ol/layer";
-import { Stroke, Style } from "ol/style.js";
 import { Vector as VectorSource } from "ol/source";
 import { GeoJSON } from "ol/format";
+import { Vector as VectorLayer } from "ol/layer";
+import { Stroke, Style } from "ol/style.js";
+import { fromLonLat, toLonLat } from "ol/proj";
 
-const MapComponent = ({ centerCoordinate, setMapInstance, style }) => {
+const SelectDataFrameMap = ({ centerCoordinate, setMapInstance, style }) => {
   const mapRef = useRef(null);
   const mapInstance = useRef(null);
   const vectorLayer = useRef(null);
 
   useEffect(() => {
+    console.log(centerCoordinate);
+    const osmLayer = new TileLayer({
+      source: new OSM(),
+      zIndex: 0,
+    });
+
     vectorLayer.current = new VectorLayer({
       source: new VectorSource(),
       style: new Style({
@@ -27,26 +33,25 @@ const MapComponent = ({ centerCoordinate, setMapInstance, style }) => {
 
     mapInstance.current = new Map({
       target: mapRef.current,
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-        vectorLayer.current,
-      ],
+      layers: [osmLayer, vectorLayer.current],
       view: new View({
         center: centerCoordinate,
         zoom: 15,
       }),
+      controls: [],
     });
 
-    const updateCenter = () => {
+    setMapInstance(mapInstance.current);
+
+    const updatePolygon = () => {
       const view = mapInstance.current.getView();
       const centerCoordinate = view.getCenter();
+      console.log(centerCoordinate);
       const lonLat = toLonLat(centerCoordinate);
       const [lon, lat] = lonLat;
-      const coords3857 = fromLonLat([lon, lat]);
-      console.log(coords3857);
+      console.log(lon, lat);
 
+      const coords3857 = fromLonLat([lon, lat]);
       const squareDist = 1000; // 1 km
 
       const geojsonObject = {
@@ -81,17 +86,15 @@ const MapComponent = ({ centerCoordinate, setMapInstance, style }) => {
       vectorSource.addFeatures(new GeoJSON().readFeatures(geojsonObject));
     };
 
-    // Add event listener to map's view for 'change:center'
-    mapInstance.current.getView().on("change:center", updateCenter);
+    mapInstance.current.on("change:center", updatePolygon);
 
     return () => {
-      // Clean up event listener when component unmounts
-      mapInstance.current.getView().un("change:center", updateCenter);
+      mapInstance.current.un("change:center", updatePolygon);
       mapInstance.current.setTarget(null);
     };
-  }, []);
+  }, [centerCoordinate, setMapInstance]);
 
-  return <div ref={mapRef} style={{ width: "100%", height: "400px" }} />;
+  return <div ref={mapRef} style={style} />;
 };
 
-export default MapComponent;
+export default SelectDataFrameMap;
