e96b102733d10527460b92a075b413b4929aabd4
loopercamera <fabian.gross@students.fhnw.ch>
2024-05-31 12:06:29 +0200

    change background map

--- frontend/src/GameMap.js
+++ frontend/src/GameMap.js
@@ -1,80 +1,112 @@
-import React, { useRef, useEffect } from 'react';
-import { Map, View } from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
-import GeoJSON from 'ol/format/GeoJSON';
-import Style from 'ol/style/Style';
-import Stroke from 'ol/style/Stroke';
-import VectorLayer from 'ol/layer/Vector';
-import VectorSource from 'ol/source/Vector';
-import { Select, DragPan, MouseWheelZoom } from 'ol/interaction';
+import React, { useRef, useEffect } from "react";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+import GeoJSON from "ol/format/GeoJSON";
+import Style from "ol/style/Style";
+import Stroke from "ol/style/Stroke";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import { Select, DragPan, MouseWheelZoom } from "ol/interaction";
+import XYZ from "ol/source/XYZ.js";
 
-import geoJsonData from './data/street_data.json'
+import geoJsonData from "./data/street_data.json";
 
-const GameMap = ({ centerCoordinate, setMapInstance, fetchData, setFetchData, startStreetList, setStartStreetList, askedStreet, setAskedStreet, doneStreetList, setDoneStreetList, falseTry, setFalseTry, centerBoxCoordinate, negativeScore, setNegativeScore, borderBox, style }) => {
+const GameMap = ({
+  centerCoordinate,
+  setMapInstance,
+  fetchData,
+  setFetchData,
+  startStreetList,
+  setStartStreetList,
+  askedStreet,
+  setAskedStreet,
+  doneStreetList,
+  setDoneStreetList,
+  falseTry,
+  setFalseTry,
+  centerBoxCoordinate,
+  negativeScore,
+  setNegativeScore,
+  borderBox,
+  style,
+}) => {
   const mapRef = useRef(null);
   const mapInstance = useRef(null);
 
   // Funktion die alle Strassennamen extrahiert
   const extractNamesFromFeatures = (features) => {
-    return features.map(feature => feature.get('name')).filter(name => name !== undefined);
+    return features
+      .map((feature) => feature.get("name"))
+      .filter((name) => name !== undefined);
   };
-  
+
   useEffect(() => {
     // Definiert den Import der Strassendaten in die Karte
     const geoJsonSource = new VectorSource({
       features: new GeoJSON({
-        dataProjection: 'EPSG:4326',
-        featureProjection: 'EPSG:3857',
+        dataProjection: "EPSG:4326",
+        featureProjection: "EPSG:3857",
       }).readFeatures(geoJsonData),
     });
 
     // Definiert den Strassenlayer
     const StreetLayer = new VectorLayer({
       source: geoJsonSource,
-      style: function(feature) {
-        const featureName = feature.get('name');
+      style: function (feature) {
+        const featureName = feature.get("name");
         if (featureName && doneStreetList.includes(featureName)) {
           // Wenn der Name in doneStreetList enthalten ist, grüne Linien
           return new Style({
             stroke: new Stroke({
-              color: '#558800', //rot
+              color: "#558800", //rot
               width: 3,
             }),
           });
-        } if (featureName && falseTry.includes(featureName)) {
+        }
+        if (featureName && falseTry.includes(featureName)) {
           // Wenn der Name in falseTry enthalten ist, grüne Linien
           return new Style({
             stroke: new Stroke({
-              color: '#CC3333', //rot
-              width: 3, 
+              color: "#CC3333", //rot
+              width: 3,
             }),
           });
         } else {
           // Wenn der Name nicht in doneStreetList enthalten ist, rote Linien
           return new Style({
             stroke: new Stroke({
-              color: '#3388CC', //blau
+              color: "#3388CC", //blau
               width: 3,
             }),
           });
         }
-      }
+      },
     });
+
+    const key = "jKNXozwbk19J7hN8HWqH";
 
     // Inhalte der Karte
     mapInstance.current = new Map({
       target: mapRef.current,
       layers: [
         new TileLayer({
-          source: new OSM(),
+          source: new XYZ({
+            url:
+              "https://api.maptiler.com/maps/satellite/{z}/{x}/{y}.jpg?key=" +
+              key,
+            tileSize: 512,
+            maxZoom: 20,
+          }),
         }),
-        StreetLayer],
+
+        StreetLayer,
+      ],
       view: new View({
         center: centerBoxCoordinate,
         zoom: 15,
         minZoom: 15,
-        extent: borderBox
+        extent: borderBox,
       }),
       controls: [],
       interactions: [new Select(), new DragPan(), new MouseWheelZoom()],
@@ -84,19 +116,26 @@ const GameMap = ({ centerCoordinate, setMapInstance, fetchData, setFetchData, st
     const handlePointerMove = (event) => {
       // Prüfen ob am gehovertem Ort ein Feature ist
       const pixel = mapInstance.current.getEventPixel(event.originalEvent);
-      const feature = mapInstance.current.forEachFeatureAtPixel(pixel, (feature) => {
-        return feature;
-      });
-      StreetLayer.getSource().getFeatures().forEach((feature) => {
-        feature.setStyle(null);
-      });
-      if (feature) {feature.setStyle(
-        new Style({
-          stroke: new Stroke({
-            color: 'blue',
-            width: 5,
-          }),
-        }));
+      const feature = mapInstance.current.forEachFeatureAtPixel(
+        pixel,
+        (feature) => {
+          return feature;
+        }
+      );
+      StreetLayer.getSource()
+        .getFeatures()
+        .forEach((feature) => {
+          feature.setStyle(null);
+        });
+      if (feature) {
+        feature.setStyle(
+          new Style({
+            stroke: new Stroke({
+              color: "blue",
+              width: 5,
+            }),
+          })
+        );
       }
     };
 
@@ -104,52 +143,78 @@ const GameMap = ({ centerCoordinate, setMapInstance, fetchData, setFetchData, st
     const handleClick = (event) => {
       // Funktion um nächste Strasse zu erfragen
       const handleNextStreet = () => {
-          if (startStreetList.length > 0) {
-              // Move the first street to the end of the list
-              const [firstStreet, ...restStreets] = startStreetList;
-              setStartStreetList([...restStreets, firstStreet]);
-              // Set the new first street as askedStreet
-              setAskedStreet(restStreets[0]);
-          }
+        if (startStreetList.length > 0) {
+          // Move the first street to the end of the list
+          const [firstStreet, ...restStreets] = startStreetList;
+          setStartStreetList([...restStreets, firstStreet]);
+          // Set the new first street as askedStreet
+          setAskedStreet(restStreets[0]);
+        }
       };
-  
+
       // Prüfen ob am geklickten Ort ein Feature ist
       const pixel = mapInstance.current.getEventPixel(event.originalEvent);
-      const feature = mapInstance.current.forEachFeatureAtPixel(pixel, (feature) => {
+      const feature = mapInstance.current.forEachFeatureAtPixel(
+        pixel,
+        (feature) => {
           return feature;
-      });
+        }
+      );
       if (feature) {
-          // Hier ergänzen was passiert, wenn man auf eine Linie klickt
-          const featureName = feature.get('name');
-          const featureStyle = feature.getStyle();
-  
-          if (featureName === askedStreet && featureStyle && featureStyle.getStroke().getColor() === 'blue') {
-              setFalseTry([])
-  
-              // Aktualisiere startStreetList und doneStreetList
-              setStartStreetList(prevList => prevList.filter(name => name !== askedStreet));
-              setDoneStreetList(prevList => [...prevList, askedStreet]);
-              handleNextStreet();
-          } else {
-              setFalseTry(prevList => [...prevList, featureName]);
-  
-              // Wenn der Name weder in doneStreetList noch in falseTry ist, erhöhe negativeScore
-              if (!doneStreetList.includes(featureName) && !falseTry.includes(featureName)) {
-                  setNegativeScore(prevScore => prevScore + 1);
-              }
+        // Hier ergänzen was passiert, wenn man auf eine Linie klickt
+        const featureName = feature.get("name");
+        const featureStyle = feature.getStyle();
+
+        if (
+          featureName === askedStreet &&
+          featureStyle &&
+          featureStyle.getStroke().getColor() === "blue"
+        ) {
+          setFalseTry([]);
+
+          // Aktualisiere startStreetList und doneStreetList
+          setStartStreetList((prevList) =>
+            prevList.filter((name) => name !== askedStreet)
+          );
+          setDoneStreetList((prevList) => [...prevList, askedStreet]);
+          handleNextStreet();
+        } else {
+          setFalseTry((prevList) => [...prevList, featureName]);
+
+          // Wenn der Name weder in doneStreetList noch in falseTry ist, erhöhe negativeScore
+          if (
+            !doneStreetList.includes(featureName) &&
+            !falseTry.includes(featureName)
+          ) {
+            setNegativeScore((prevScore) => prevScore + 1);
           }
+        }
       }
     };
-  
-    mapInstance.current.on('pointermove', handlePointerMove);
-    mapInstance.current.on('click', handleClick);
+
+    mapInstance.current.on("pointermove", handlePointerMove);
+    mapInstance.current.on("click", handleClick);
 
     return () => {
-      mapInstance.current.un('pointermove', handlePointerMove);
-      mapInstance.current.un('click', handleClick);
+      mapInstance.current.un("pointermove", handlePointerMove);
+      mapInstance.current.un("click", handleClick);
       mapInstance.current.setTarget(null);
     };
-  }, [centerCoordinate, centerBoxCoordinate, fetchData, setFetchData, askedStreet, setAskedStreet, setDoneStreetList, startStreetList, setStartStreetList, doneStreetList, falseTry, setFalseTry, setNegativeScore ]);
+  }, [
+    centerCoordinate,
+    centerBoxCoordinate,
+    fetchData,
+    setFetchData,
+    askedStreet,
+    setAskedStreet,
+    setDoneStreetList,
+    startStreetList,
+    setStartStreetList,
+    doneStreetList,
+    falseTry,
+    setFalseTry,
+    setNegativeScore,
+  ]);
 
   useEffect(() => {
     setMapInstance(mapInstance.current);
@@ -160,20 +225,20 @@ const GameMap = ({ centerCoordinate, setMapInstance, fetchData, setFetchData, st
       // Load data from JSON file
       const geoJsonSource = new VectorSource({
         features: new GeoJSON({
-          dataProjection: 'EPSG:4326',
-          featureProjection: 'EPSG:3857',
+          dataProjection: "EPSG:4326",
+          featureProjection: "EPSG:3857",
         }).readFeatures(geoJsonData),
       });
-  
+
       // Extract street names and set the start street list
       const names = extractNamesFromFeatures(geoJsonSource.getFeatures());
       setStartStreetList(names);
-  
+
       // Set the first street as askedStreet if the names array is not empty
       if (names.length > 0) {
         setAskedStreet(names[0]);
       }
-  
+
       // Set fetchData back to false to prevent reloading data
       setFetchData(false);
     }
